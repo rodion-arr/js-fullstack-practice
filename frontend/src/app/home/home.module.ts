8faa60c4d0b4fa7612e5806b5e7c4cd1d1cbde28
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-01-22 00:05:51 +0200

    #4 - add banner component

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { MainBannerComponent } from './main-banner/main-banner.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, MainBannerComponent],
   imports: [
     SharedModule,
     HomeRoutingModule
