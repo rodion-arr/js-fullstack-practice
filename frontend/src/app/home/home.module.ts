41ae68ffe0c73d4d765cfc62e1e6f5f24273eddb
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-01-24 00:47:09 +0200

    #4 - Add plates component

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home/home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MainBannerComponent } from './main-banner/main-banner.component';
 import { VideoBlockComponent } from './video-block/video-block.component';
+import { PlatesComponent } from './plates/plates.component';
 
 @NgModule({
-  declarations: [HomeComponent, MainBannerComponent, VideoBlockComponent],
+  declarations: [HomeComponent, MainBannerComponent, VideoBlockComponent, PlatesComponent],
   imports: [
+    CommonModule,
     SharedModule,
     HomeRoutingModule
   ],
