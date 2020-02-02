6a0cd0dbfbc8e89255302a299bb94222b0c79fd0
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-02-02 21:48:13 +0200

    #15 - Add lazy-loaded module for static pages
    - Add new lazy loaded module
    - Add routes to menu items

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'home'},
   {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: 'static', loadChildren: () => import('./static-pages/static-pages.module').then(m => m.StaticPagesModule)},
 ];
 
 @NgModule({
