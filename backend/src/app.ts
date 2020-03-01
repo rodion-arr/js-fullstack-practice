fd132d2a2d55b8a7ba571dfb6945e5f3f0d4e879
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-03-02 01:10:11 +0200

    Add basic NodeJS setup with TypeScript

---
+++ backend/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+// Controllers (route handlers)
+import * as apiController from "./controllers/api";
+
+// Create Express server
+const app = express();
+
+// Express configuration
+app.set("port", process.env.PORT || 3000);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/**
+ * API examples routes.
+ */
+app.get("/api", apiController.getApi);
+
+export default app;
