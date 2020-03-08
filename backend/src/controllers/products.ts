953d49380ad2665410229ebfeabac412179f5c95
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-03-09 00:50:50 +0200

    Backed | #43 - update product images URL structure

--- backend/src/controllers/products.ts
+++ backend/src/controllers/products.ts
@@ -2,13 +2,23 @@
 
 import { Response, Request } from 'express';
 import { Product } from '../models/Product';
+import { AddressInfo } from 'net';
 
 /**
  * GET /products
  */
 export const getProducts = async (req: Request, res: Response) => {
     try {
+        const address = req.socket.address() as AddressInfo;
+        const serviceUrl = `${req.protocol}://${req.hostname}:${address.port}`;
         const productList = await Product.find();
+
+        // update product urls with hostname
+        for (const product of productList) {
+            product.previewImage = `${serviceUrl}${product.previewImage}`;
+            product.detailImage = `${serviceUrl}${product.detailImage}`;
+        }
+
         res.json({
             status: true,
             message: 'Product list retrieved successfully',
--- backend/test/src/controllers/products.test.ts
+++ backend/test/src/controllers/products.test.ts
@@ -1,13 +1,15 @@
 import request from 'supertest';
 import app from '../../../src/app';
-import { Product, ProductDocument } from '../../../src/models/Product';
+import { Product } from '../../../src/models/Product';
 
 describe('products controller', () => {
     describe('GET /products', () => {
         it('should return correct data object', (done) => {
             const productSpy = jest.spyOn(Product, 'find').mockResolvedValue([
                 new Product({
-                    'shortTitle': 'TestProduct'
+                    'shortTitle': 'TestProduct',
+                    'previewImage': '/path/to/preview/img',
+                    'detailImage': '/path/to/detail/img'
                 })
             ]);
 
@@ -17,6 +19,14 @@ describe('products controller', () => {
                     expect(response.body.status).toBe(true);
                     expect(response.body.message).toBe('Product list retrieved successfully');
                     expect(response.body.data.products[0].shortTitle).toBe('TestProduct');
+
+                    const previewImage = response.body.data.products[0].previewImage;
+                    const detailImage = response.body.data.products[0].detailImage;
+                    expect(previewImage).toContain('/path/to/preview/img');
+                    expect(previewImage).toContain('http');
+                    expect(detailImage).toContain('/path/to/detail/img');
+                    expect(detailImage).toContain('http');
+
                     productSpy.mockClear();
                     done();
                 });
