867cd8044a501acca347104b7ae9879f45a9f4fa
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-04-17 23:29:46 +0300

    #96 - Use clover format for frontend code coverage

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -11,22 +11,16 @@ module.exports = function (config) {
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-junit-reporter')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/f'),
-      reports: ['html', 'lcovonly', 'text-summary'],
+      reports: ['text-summary', 'clover'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'junit'],
-    junitReporter: {
-      outputDir: 'reports', // results will be saved as $outputDir/$browserName.xml
-      outputFile: 'junit.xml', // if included, results will be saved as $outputDir/$browserName/$outputFile
-      useBrowserName: false // add browser name to report and classes names
-    },
+    reporters: ['progress', 'coverage-istanbul'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
