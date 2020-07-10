0208590b28ddd5da429985b6f1cc38da752ba5b7
Rodion Abdurakhimov <rodion.arr.web@gmail.com>
2020-07-10 18:57:12 +0300

    Add timestamps to Winston logger

--- backend/src/util/logger.ts
+++ backend/src/util/logger.ts
@@ -1,7 +1,12 @@
 import winston from 'winston';
-import { Logger } from 'winston';
+import { Logger, format } from 'winston';
+const { combine, timestamp } = format;
 
 const logger: Logger = winston.createLogger({
+    format: combine(
+        timestamp(),
+        format.json()
+    ),
     transports: [
         new winston.transports.Console({ level: process.env.NODE_ENV === 'production' ? 'info' : 'debug' }),
     ]
